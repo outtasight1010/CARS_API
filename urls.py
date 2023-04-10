bf88f6f8adbe289f964b5dc422ee3419889fc2f2
outtasight1010 <126138474+outtasight1010@users.noreply.github.com>
2023-04-09 23:16:46 -0400

    Add files via upload

--- urls.py
+++ urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/cars/', include("cars.urls")),
+    path('api/cars/', include('cars.urls')),
 ]
